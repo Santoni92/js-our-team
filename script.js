d7342690ebd45b635a76c5915d2ab6a98eb48c8f
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-03-21 16:58:28 +0100

    modifica

--- script.js
+++ script.js
@@ -10,25 +10,36 @@ const wayneBarnett = {
 const angelaCarrol = {
     nome: "Angela",
     ruolo: "chief editor",
-    img: "img/wayne-barnett-founder-console.jpg"
+    img: "img/angela-caroll-chief-editor.jpg"
 };
 
 const walterGordon = {
     nome: "Walter",
     ruolo: "office manager",
-    img: "img/wayne-barnett-founder-console.jpg"
+    img: "img/walter-gordon-office-manager.jpg"
 };
 
 const angelaLopez = {
     nome: "Angela",
     ruolo: "social media manager",
-    img: "img/wayne-barnett-founder-console.jpg"
+    img: "img/angela-lopez-social-media-manager.jpg"
+};
+
+const scottEstrada = {
+    nome: "Scott",
+    ruolo: "developer",
+    img: "img/scott-estrada-developer.jpg"
+};
+
+const barbaraRamos = {
+    nome: "barbara",
+    ruolo: "graphic designer",
+    img: "img/barbara-ramos-graphic-designer.jpg"
 };
 
 
 
-
-const team = [wayneBarnett,angelaCarrol,walterGordon,angelaLopez];  //array di oggetti
+const team = [wayneBarnett,angelaCarrol,walterGordon,angelaLopez,scottEstrada,barbaraRamos];  //array di oggetti
 
 console.log(team);
 
@@ -39,10 +50,42 @@ for(let key in wayneBarnett)
 
 //Una volta definita la struttura dati, prendendo come riferimento la card di esempio presente nell'html, stampare dinamicamente una card per ogni membro del team.
 
+//const card = document.createElement('div');
+const card = [];    //creo un array per salvarmi i riferimenti ai vari div che conterranno l'immagine,il nome ed la professione di ciascun membro del team
+const nome = [];    //array per salvarmi i riferimenti ai vari h3 dei nomi
+const ruolo = [];   //array per salvarmi i riferimenti ai vari p in cui ci sarà scritta la professione
+const img = [];     //array per salvarmi i riferimenti alle varie immagini 
+for(let i = 0; i < team.length;i++)
+{
+    card.push(document.createElement('div'));
+    nome.push(document.createElement('h3'));
+    ruolo.push(document.createElement('p'));
+    img.push(document.createElement('img'));
+}
 
-const card = document.createElement('div');
+for(let i = 0; i < team.length;i++)
+{
+    document.querySelector('.team-container').appendChild(card[i]);
 
-const nome = document.createElement('h3');
+}
+
+for(let i = 0; i < team.length;i++)
+{
+    nome[i].innerText = team[i].nome;
+    ruolo[i].innerText = team[i].ruolo;
+    img[i].setAttribute('src',wayneBarnett.img);
+}
+
+for(let i = 0; i < team.length; i++)
+{
+    card[i].appendChild(img[i]);
+    card[i].appendChild(nome[i]);
+    card[i].appendChild(ruolo[i]);
+
+}
+
+
+/*const nome = document.createElement('h3');
 nome.innerText = wayneBarnett.nome;
 
 const ruolo = document.createElement('p');
@@ -55,7 +98,7 @@ document.querySelector('.team-container').appendChild(card);
 card.appendChild(img);
 card.appendChild(nome);
 card.appendChild(ruolo);
-
+*/
 
 
 
